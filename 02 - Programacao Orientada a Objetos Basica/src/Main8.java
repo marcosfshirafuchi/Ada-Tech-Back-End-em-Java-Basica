3e86b4258408faa39739d993422e18516a427480
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-02-01 21:50:27 -0300

    Para virar mestre

---
+++ 02 - Programacao Orientada a Objetos Basica/src/Main8.java
@@ -0,0 +1,29 @@
+import Animais.Cachorro;
+import Animais.Gato;
+import Animais.Passaro;
+
+public class Main8 {
+    public static void main(String[] args) {
+        Cachorro cachorro1 = new Cachorro("Rex","Marron",25,5.5,5,"nada");
+        Gato gato1 = new Gato("Felix","Preto",4.5);
+        Passaro passaro1 = new Passaro("Frajola","Azul",0.5);
+
+        cachorro1.soar();
+        gato1.soar();
+        passaro1.soar();
+
+        int inteiro; //int, long, byte = ;
+        float $$; //double e float = 0.0
+        boolean $87; //boolean tem como default false
+        char ch; // char é vazio
+        String teste; // valor default de qualquer objeto é null
+
+        double d = 5.5d;
+        float f = 3.00f;
+
+        float x = f + (float) d;
+        System.out.println(x);
+
+        int a = 23_456_789;
+    }
+}
